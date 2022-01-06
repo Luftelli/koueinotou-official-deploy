a06dbebb35d5072c0c2c00e73e6c41c6b0274725
Cdec <cdecrement@gmail.com>
2022-01-07 01:31:13 +0900

    [update] フォント変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      body: [
+        'Noto Sans Japanese'
+      ]
+    },
     extend: {},
   },
   plugins: [],
