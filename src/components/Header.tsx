8c25c84b0ad104021099fa3e5999fab3d0fb370f
Cdec <cdecrement@gmail.com>
2022-01-07 00:15:46 +0900

    [add] Luftelliサイトへのリンクを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,11 @@ const Header: React.FC<HeaderProps> = ({ pageTitle, pageDescription, className =
         <meta name='description' content={description} />
       </Helmet>
 
-      <nav className='flex justify-center w-full py-4'></nav>
+      <nav className='flex justify-center w-full py-2'>
+          <div className='flex flex-grow max-w-screen-lg px-4'>
+          <a href='https://luftelli.com'><p className='flex-none text-base'>Luftelli</p></a>
+          </div>
+      </nav>
     </header>
   );
 };
