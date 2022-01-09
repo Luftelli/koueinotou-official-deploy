7ae5158f01b6ef0ca7a10d93ac17eb92abe253ef
Cdec <cdecrement@gmail.com>
2022-01-10 04:07:19 +0900

    [update] Netlifyリダイレクト設定修正

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 exports.createPages = ({ actions }) => {
     const { createRedirect } = actions;
     createRedirect({
-        fromPath: "https://koueinotou.netlify.app/*",
-        toPath: "https://koueinotou.luftelli.com/:splat",
+        fromPath: "https://koueinotou.netlify.app",
+        toPath: "https://koueinotou.luftelli.com",
         statusCode: 301,
     });
 }
