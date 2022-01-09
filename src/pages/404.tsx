13077709e0ce08c8bf83dffe24396b6939f11188
Cdec <cdecrement@gmail.com>
2022-01-10 03:46:31 +0900

    [update] 404ページ追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,54 +1,19 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import Layout from '../components/Layout';
+import Section from '../components/Section';
+import Button from '../components/Button';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Layout pageTitle='開発状況' pageDescription='開発の状況'>
+      <h1 className='text-4xl'>ページが見つかりません</h1>
 
-export default NotFoundPage
+      <Section className='gap-y-4'>
+        <p>お探しのページが見つかりませんでした……</p>
+        <Button link='/'>トップページに戻る</Button>
+      </Section>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
