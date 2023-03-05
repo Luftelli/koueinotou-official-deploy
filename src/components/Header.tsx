47fe16653af246c1b378e2d6b20a1042fbcf9b26
Cdec <cdecrement@gmail.com>
2023-03-06 01:03:04 +0900

    [update] ヘッダーにトップページへのリンク追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 type HeaderProps = {
@@ -8,8 +10,11 @@ const Header: React.FC<HeaderProps> = ({ className = '' }) => (
   // ページタイトルが指定されない場合はトップページとする
   <header className={className}>
     <nav className='flex justify-center w-full py-2'>
-      <div className='flex flex-grow max-w-screen-lg px-4'>
-        <a href='https://luftelli.com'>
+      <div className='flex flex-grow max-w-screen-lg px-4 justify-between'>
+        <Link to='/' className='tooltip' data-tip='トップページへ'>
+          <StaticImage src='../images/ctm_logo_v1.png' height={32} alt='トップページへ' />
+        </Link>
+        <a href='https://luftelli.com' className='tooltip' data-tip='サークルページへ'>
           <p className='flex-none text-base'>Luftelli</p>
         </a>
       </div>
